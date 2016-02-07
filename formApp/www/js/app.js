2a793f97928698ab51d06aa04e75e4766e9d886d
Julian Harley <julian.harley@pancentric.com>
2016-02-07 15:21:01 +0000

    add default Birthday service

--- formApp/www/js/app.js
+++ formApp/www/js/app.js
@@ -1,9 +1,99 @@
 // Ionic Starter App
-
+//
+// http://onehungrymind.com/angularjs-dynamic-templates/
+//
+var db = new PouchDB('birthdays');
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
+
+function BirthdayService($q) {
+    var _db;
+    // We'll need this later.
+    var _birthdays;
+    return {
+        initDB: initDB,
+        getAllBirthdays: getAllBirthdays,
+        addBirthday: addBirthday,
+        updateBirthday: updateBirthday,
+        deleteBirthday: deleteBirthday
+    };
+    function initDB() {
+        // Creates the database or opens if it already exists
+        _db = new PouchDB('birthdays', {adapter: 'websql'});
+    };
+
+    function addBirthday(birthday) {
+          console.log('clik add birthday');
+          console.log(birthday);
+          return $q.when(_db.post(birthday));
+    };
+
+    function updateBirthday(birthday) {
+        return $q.when(_db.put(birthday));
+    };
+
+    function deleteBirthday(birthday) {
+        return $q.when(_db.remove(birthday));
+    };
+
+    function getAllBirthdays() {
+        if (!_birthdays) {
+           return $q.when(_db.allDocs({ include_docs: true}))
+                .then(function(docs) {
+
+                    // Each row has a .doc object and we just want to send an
+                    // array of birthday objects back to the calling controller,
+                    // so let's map the array to contain just the .doc objects.
+                    _birthdays = docs.rows.map(function(row) {
+                        // Dates are not automatically converted from a string.
+                        row.doc.Date = new Date(row.doc.Date);
+                        return row.doc;
+                    });
+                    // Listen for changes on the database.
+                    _db.changes({ live: true, since: 'now', include_docs: true})
+                       .on('change', onDatabaseChange);
+
+                    return _birthdays;
+                });
+        } else {
+            // Return cached data as a promise
+            return $q.when(_birthdays);
+        }
+    };
+
+    function onDatabaseChange(change) {
+        var index = findIndex(_birthdays, change.id);
+        var birthday = _birthdays[index];
+
+        if (change.deleted) {
+            if (birthday) {
+                _birthdays.splice(index, 1); // delete
+            }
+        } else {
+            if (birthday && birthday._id === change.id) {
+                _birthdays[index] = change.doc; // update
+            } else {
+                _birthdays.splice(index, 0, change.doc) // insert
+            }
+        }
+    }
+
+    function findIndex(array, id) {
+      var low = 0, high = array.length, mid;
+      while (low < high) {
+        mid = (low + high) >>> 1;
+        array[mid]._id < id ? low = mid + 1 : high = mid
+      }
+      return low;
+    }
+
+
+}
+
 var app = angular.module('starter', ['ionic']);
+app.factory('BirthdayService', BirthdayService);
+
 
 app.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -48,8 +138,17 @@ app.factory('TemplateService', function ($http, URL) {
 
 // template handling:
 
-app.controller('ContentCtrl', function (DataService) {
+app.controller('ContentCtrl', function ($scope, $ionicPlatform, DataService, BirthdayService) {
     var ctrl = this;
+    var vm = this;
+    // Initialize the database.
+    $ionicPlatform.ready(function() {
+        BirthdayService.initDB();
+        // Get all birthday records from the database.
+        BirthdayService.getAllBirthdays().then(function(birthdays) {
+        vm.birthdays = birthdays;
+      });
+    });
     ctrl.content = [];
     ctrl.fetchContent = function () {
         DataService.getData().then(function (result) {
@@ -57,6 +156,11 @@ app.controller('ContentCtrl', function (DataService) {
         });
     };
     ctrl.fetchContent();
+      // save button action
+    $scope.saveBirthday = function() {
+        BirthdayService.addBirthday({'new':'mew'});
+        //BirthdayService.updateBirthday($scope.birthday);
+    };
 });
 
 app.directive('contentItem', function ($compile, TemplateService) {
